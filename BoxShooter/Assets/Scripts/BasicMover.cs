ea5cfb66a6e65997729a779dad92340c8e373fcd
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2016-08-03 21:26:57 +0300

    Add first script.

---
+++ BoxShooter/Assets/Scripts/BasicMover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Basic mover.
+/// </summary>
+public class BasicMover : MonoBehaviour {
+
+	/// <summary>
+	/// The spin speed.
+	/// </summary>
+	public float spinSpeed = 180.0f;
+
+	// Update is called once per frame
+	void Update () {
+	
+		gameObject.transform.Rotate (Vector3.up * spinSpeed * Time.deltaTime);
+
+	}
+}
